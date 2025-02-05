550e8365b4152e603b1a12a92a9c4d07216a3edf
VandurTheWizard <TheWizardVandur@gmail.com>
2025-02-05 02:42:54 +0100

    Actu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Scripting.APIUpdating;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [Header("Player Settings")]
-    [SerializeField] private float velocidad = 5f;
-    [SerializeField] private float fuerzaSalto = 5f;
-    [SerializeField] private float sensiblidadCamara = 2f;
+    [Header("General")]
+    public float gravityScale = -20f;
+    [Header("Movement")]
+    public float walkSpeed = 5f;
+    public float runSpeed = 10f;
+    [Header("Camera")]
+    public float rotationSensibility = 30f;
+    [Header("Jump")]
+    public float jumpHeight = 1.9f;
+    [Header("References")]
+    public Transform objectToRotate; // El objeto que quieres rotar
 
-    private Rigidbody rb;
+    private float cameraVerticalAngle;
+    private Vector3 moveInput = Vector3.zero;
+    private Vector3 rotationInput = Vector3.zero;
+    private CharacterController characterController;
+    private Vector2 move;
+    private Vector2 look;
+    private bool isSprinting;
+    private bool isJumping;
 
-    private void Start()
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
     }
-    public void OnMove(InputAction.CallbackContext context){
-        Vector2 input = context.ReadValue<Vector2>();
-        Vector3 movimiento = new Vector3(input.x, 0, input.y) * velocidad * Time.deltaTime;
-        rb.MovePosition(transform.position + transform.TransformDirection(movimiento));
+
+    private void Update()
+    {
+        Look();
+        Move();
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        look = context.ReadValue<Vector2>();
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        isSprinting = context.ReadValueAsButton();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        isJumping = context.ReadValueAsButton();
+    }
+
+    private void Move()
+    {
+        if (characterController.isGrounded)
+        {
+            moveInput = new Vector3(move.x, 0, move.y);
+            moveInput = Vector3.ClampMagnitude(moveInput, 1f);
+
+            if (isSprinting)
+            {
+                moveInput = transform.TransformDirection(moveInput) * runSpeed;
+            }
+            else
+            {
+                moveInput = transform.TransformDirection(moveInput) * walkSpeed;
+            }
+
+            if (isJumping)
+            {
+                moveInput.y = Mathf.Sqrt(jumpHeight * -2f * gravityScale);
+            }
+        }
+
+        moveInput.y += gravityScale * Time.deltaTime;
+        characterController.Move(moveInput * Time.deltaTime);
+    }
+
+    private void Look()
+    {
+        rotationInput = new Vector3(-look.y, look.x, 0) * rotationSensibility * Time.deltaTime;
+        cameraVerticalAngle += rotationInput.x;
+        cameraVerticalAngle = Mathf.Clamp(cameraVerticalAngle, -90f, 90f);
+
+        objectToRotate.localEulerAngles = new Vector3(cameraVerticalAngle, 0, 0);
+        transform.Rotate(Vector3.up * rotationInput.y);
     }
 }
